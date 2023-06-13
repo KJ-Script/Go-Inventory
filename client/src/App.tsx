bdf82c23a818fab3add5bcaa5536994b3adac586
snowstorm47 <contact.abelend@gmail.com>
2023-06-13 12:35:08 +0300

    started the home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <p className="text-red-500 w-full">Test code to check if tailwind works</p>
+      <p className="text-red-500 w-full">Test code to check if tailwind works, yay it works </p>
     </>
   )
 }
